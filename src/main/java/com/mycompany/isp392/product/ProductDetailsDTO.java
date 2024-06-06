db7c67f4459a89c8ad438d1e32342f5fca7b6e32
LAPTOPTTN\TTNHAT <nhatttse183879@fpt.edu.vn>
2024-06-06 22:08:31 +0700

    ProductDetailsDTO

---
+++ src/main/java/com/mycompany/isp392/product/ProductDetailsDTO.java
@@ -0,0 +1,106 @@
+package com.mycompany.isp392.product;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.Date;
+
+/**
+ *
+ * @author TTNHAT
+ */
+public class ProductDetailsDTO {
+
+    private int productID;
+    private String color;
+    private String size;
+    private int stockQuantity;
+    private int price;
+    private Date importDate;
+    private String image;
+    private boolean status;
+
+    public ProductDetailsDTO() {
+    }
+
+    public ProductDetailsDTO(int productID, String color, String size, int stockQuantity, int price, Date importDate, String image, boolean status) {
+        this.productID = productID;
+        this.color = color;
+        this.size = size;
+        this.stockQuantity = stockQuantity;
+        this.price = price;
+        this.importDate = importDate;
+        this.image = image;
+        this.status = status;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Date getImportDate() {
+        return importDate;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImportDate(Date importDate) {
+        this.importDate = importDate;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDetailsDTO{" + "productID=" + productID + "color=" + color + ", size=" + size + ", stockQuantity=" + stockQuantity + ", price=" + price + ", importDate=" + importDate + ", image=" + image + ", status=" + status + '}';
+    }
+}
