4e9ee4253601eb501363e8a05eaa08fbae063b49
Tim Tran <tim.tran@bufferzone.com.au>
2024-06-06 21:57:06 +0700

    ProductDAO and ProductController

---
+++ src/main/java/com/mycompany/isp392/controllers/DeleteController.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.isp392.controllers;
+
+import com.mycompany.isp392.product.ProductDAO;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author tuan tran
+ */
+public class DeleteController extends HttpServlet {
+    private static final String ERROR = "manageProduct.jsp";
+    private static final String SUCCESS = "manageProduct.jsp";
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+         String url = ERROR;
+        try {
+            String productID = request.getParameter("productID");
+            ProductDAO productDAO = new ProductDAO();
+            boolean check = productDAO.deleteProduct(productID);
+            if (check) {
+                request.setAttribute("MESSAGE", "Product deactivated successfully!");
+                url = SUCCESS;
+            }
+        } catch (SQLException e) {
+            log("Error at DeleteCloseOperation: " + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/main/java/com/mycompany/isp392/controllers/MainController.java
+++ src/main/java/com/mycompany/isp392/controllers/MainController.java
@@ -65,15 +65,6 @@ public class MainController extends HttpServlet {
     private static final String CHECKOUT = "CheckOut";
     private static final String CHECKOUT_CONTROLLER = "CheckOutController";
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
                 response.setContentType("text/html;charset=UTF-8");
