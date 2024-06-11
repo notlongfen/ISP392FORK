8378aefe95f9a2b6c91b78a23fa9ba84b7918144
Tim Tran <tim.tran@bufferzone.com.au>
2024-06-11 19:51:41 +0700

    Brand CRUD

--- src/main/java/com/mycompany/isp392/brand/BrandDAO.java
+++ src/main/java/com/mycompany/isp392/brand/BrandDAO.java
@@ -11,7 +11,10 @@ import utils.DbUtils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +22,11 @@ import java.util.ArrayList;
  */
 public class BrandDAO {
     private static final String SEARCH_BRAND = "SELECT * FROM Brands WHERE brandName LIKE ?";
-    
+    private static final String INSERT_BRAND = "INSERT INTO Brands(BrandName, status) VALUES(?,1)";
+    private static final String UPDATE_BRAND = "UPDATE Brands SET BrandName=? WHERE BrandID=?";
+    private static final String DEACTIVATE_BRAND = "UPDATE Brands SET status = 0 WHERE BrandID=?";
+    private static final String SELECT_BRAND = "SELECT BrandID, BrandName, status FROM Brands WHERE BrandID=?";
+   
     public List<BrandDTO> searchForBrand(String brandName) {
         List<BrandDTO> list = new ArrayList<>();
         Connection conn = null;
@@ -44,5 +51,83 @@ public class BrandDAO {
             e.printStackTrace();
         }
         return list;
+    }
+      public boolean addBrand(String brandName) throws SQLException {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        try {
+            conn = DbUtils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(INSERT_BRAND);
+                ptm.setString(1, brandName);
+                check = ptm.executeUpdate() > 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BrandDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return check;
+    }
+
+    public boolean updateBrand(String brandName, int brandID) throws SQLException {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        try {
+            conn = DbUtils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(UPDATE_BRAND);
+                ptm.setString(1, brandName);
+                ptm.setInt(2, brandID);
+                check = ptm.executeUpdate() > 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BrandDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return check;
+    }
+
+    public boolean deleteBrand(int brandID) throws SQLException {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        try {
+            conn = DbUtils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(DEACTIVATE_BRAND);
+                ptm.setInt(1, brandID);
+                check = ptm.executeUpdate() > 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BrandDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return check;
     }
 }
---
+++ src/main/java/com/mycompany/isp392/controllers/DeleteBrandController.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.isp392.controllers;
+
+import com.mycompany.isp392.brand.BrandDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author tuan tran
+ */
+public class DeleteBrandController extends HttpServlet {
+
+      private static final String ERROR = "manageBrand.jsp";
+    private static final String SUCCESS = "manageBrand.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        try {
+            int brandID = Integer.parseInt(request.getParameter("brandID"));
+            BrandDAO brandDAO = new BrandDAO();
+            boolean check = brandDAO.deleteBrand(brandID);
+            if (check) {
+                url = SUCCESS;
+            }
+        } catch (SQLException e) {
+            log("Error at DeactivateBrandController: " + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
